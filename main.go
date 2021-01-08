1a2da5063c5ec5462c3f148a9357a5ec45e7070f
Ivan Dolgov <id@club-is.ru>
2021-01-08 19:09:40 +0700

    Рефакторинг

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 	"strconv"
 )
 
-var dirname = "/temp"
-var dirname2 = "/temp2" + string(filepath.Separator)
+var sourceDir = string(filepath.Separator) + "temp"
+var exportDir = string(filepath.Separator) + "temp2"
+
 var months = make(map[string]string)
 
 func sortImage(file os.FileInfo) (error, string) {
-	filePath := path.Join(dirname, file.Name())
+	filePath := path.Join(sourceDir, file.Name())
 	f, err := os.Open(filePath)
 
 	if err != nil {
@@ -34,7 +35,7 @@ func sortImage(file os.FileInfo) (error, string) {
 	month := months[tm.Month().String()]
 	year := strconv.Itoa(tm.Year())
 
-	exportPath := dirname2 + year + string(filepath.Separator) + month + string(filepath.Separator) + day
+	exportPath := path.Join(exportDir, year, month, day)
 	_ = os.MkdirAll(exportPath, 0777)
 
 	dst := path.Join(exportPath, file.Name())
@@ -59,13 +60,14 @@ func init() {
 	months["November"] = "11"
 	months["December"] = "12"
 }
+
 func showHappyEnd() {
 	fmt.Println()
 	fmt.Println("/   \\          /   \\\n\\_   \\        /  __/\n _\\   \\      /  /__\n \\___  \\____/   __/\n     \\_       _/\n       | @ @  \\_\n       |\n     _/     /\\\n    /o)  (o/\\ \\_\n    \\_____/ /\n      \\____/")
 }
 
 func main() {
-	files, err := ioutil.ReadDir(dirname)
+	files, err := ioutil.ReadDir(sourceDir)
 	if err != nil {
 		panic(err)
 	}
