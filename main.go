9f3c360bc02d2ab9500ed7860fa63acbc799b38e
Ivan Dolgov <id@club-is.ru>
2021-01-11 00:01:44 +0700

    Добавлена сортировка для видео с расширением .MOV (БОЛЬШИЕ БУКВЫ!!!!) Рефакторинг

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"github.com/rwcarlsen/goexif/exif"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var sourceDir = string(filepath.Separator) + "temp"
@@ -15,35 +21,91 @@ var exportDir = string(filepath.Separator) + "temp2"
 
 var months = make(map[string]string)
 
-func sortImage(file os.FileInfo) (error, string) {
-	filePath := path.Join(sourceDir, file.Name())
-	f, err := os.Open(filePath)
+const appleEpochAdjustment = 2082844800
 
-	if err != nil {
-		_ = f.Close()
-		return err, ""
+const (
+	movieResourceAtomType   = "moov"
+	movieHeaderAtomType     = "mvhd"
+	referenceMovieAtomType  = "rmra"
+	compressedMovieAtomType = "cmov"
+)
+
+func getVideoCreationTimeMetadata(videoBuffer io.ReadSeeker) (time.Time, error) {
+	buf := make([]byte, 8)
+
+	// Traverse videoBuffer to find movieResourceAtom
+	for {
+		// bytes 1-4 is atom size, 5-8 is type
+		// Read atom
+		if _, err := videoBuffer.Read(buf); err != nil {
+			return time.Time{}, err
+		}
+
+		if bytes.Equal(buf[4:8], []byte(movieResourceAtomType)) {
+			break // found it!
+		}
+
+		atomSize := binary.BigEndian.Uint32(buf) // check size of atom
+		videoBuffer.Seek(int64(atomSize)-8, 1)   // jump over data and set seeker at beginning of next atom
 	}
 
-	x, err := exif.Decode(f)
-	if err != nil {
-		return err, ""
+	// read next atom
+	if _, err := videoBuffer.Read(buf); err != nil {
+		return time.Time{}, err
 	}
-	_ = f.Close()
 
-	tm, _ := x.DateTime()
-	day := fmt.Sprintf("%02d", tm.Day())
-	month := months[tm.Month().String()]
-	year := strconv.Itoa(tm.Year())
+	atomType := string(buf[4:8]) // skip size and read type
+	switch atomType {
+	case movieHeaderAtomType:
+		// read next atom
+		if _, err := videoBuffer.Read(buf); err != nil {
+			return time.Time{}, err
+		}
+
+		// byte 1 is version, byte 2-4 is flags, 5-8 Creation time
+		appleEpoch := int64(binary.BigEndian.Uint32(buf[4:])) // Read creation time
+
+		return time.Unix(appleEpoch-appleEpochAdjustment, 0).Local(), nil
+	case compressedMovieAtomType:
+		return time.Time{}, errors.New("Compressed video")
+	case referenceMovieAtomType:
+		return time.Time{}, errors.New("Reference video")
+	default:
+		return time.Time{}, errors.New("Did not find movie header atom (mvhd)")
+	}
+}
+
+func sortImage(f io.ReadSeeker) (time.Time, error) {
+	resultExif, err := exif.Decode(f)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	resultDateTime, err := resultExif.DateTime()
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return resultDateTime, nil
+}
+
+func moveFileToNewLocation(filePath string, fileName string, created time.Time) (string, error) {
+	day := fmt.Sprintf("%02d", created.Day())
+	month := months[created.Month().String()]
+	year := strconv.Itoa(created.Year())
 
 	exportPath := path.Join(exportDir, year, month, day)
-	_ = os.MkdirAll(exportPath, 0777)
+	err := os.MkdirAll(exportPath, 0777)
+	if err != nil {
+		return "", err
+	}
 
-	dst := path.Join(exportPath, file.Name())
+	dst := path.Join(exportPath, fileName)
 	err = os.Rename(filePath, dst)
 	if err != nil {
-		return err, ""
+		return "", err
 	}
-	return nil, dst
+	return dst, nil
 }
 
 func init() {
@@ -73,17 +135,45 @@ func main() {
 	}
 	numberFiles := len(files)
 
-	i := 0
-	for _, file := range files {
-		i++
+	for i, file := range files {
 		fmt.Print(fmt.Sprintf("%3d", int(float64(i)/float64(numberFiles)*100)) + "%| " + file.Name())
 
-		err, dstFilePath := sortImage(file)
+		filePath := path.Join(sourceDir, file.Name())
+		fd, err := os.Open(filePath)
 		if err != nil {
 			fmt.Print("\n")
 			fmt.Println(err)
 			continue
 		}
+
+		var created time.Time
+		if strings.Contains(file.Name(), ".MOV") {
+			created, err = getVideoCreationTimeMetadata(fd)
+			if err != nil {
+				fmt.Print("\n")
+				fmt.Println(err)
+				fd.Close()
+				continue
+			}
+		} else {
+			created, err = sortImage(fd)
+			if err != nil {
+				fd.Close()
+				fmt.Print("\n")
+				fmt.Println(err)
+				continue
+			}
+		}
+
+		fd.Close()
+
+		dstFilePath, err := moveFileToNewLocation(filePath, file.Name(), created)
+		if err != nil {
+			fmt.Print("\n")
+			fmt.Println(err)
+			continue
+		}
+
 		fmt.Print(" to " + dstFilePath + "\n")
 	}
 
