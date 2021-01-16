463dad23ae1231ef8129475493b2285e8bb3949a
Ivan Dolgov <id@club-is.ru>
2021-01-17 00:09:52 +0700

    Добавлена поддержка xmp файла (при редактировании camera raw) Исправлена чувствительность регистра у расширения файла. Рефакторинг.

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"trimmer.io/go-xmp/models/xmp_base"
+	"trimmer.io/go-xmp/xmp"
 )
 
 var sourceDir = string(filepath.Separator) + "temp"
@@ -30,6 +32,78 @@ const (
 	compressedMovieAtomType = "cmov"
 )
 
+func init() {
+	months["January"] = "01"
+	months["February"] = "02"
+	months["March"] = "03"
+	months["April"] = "04"
+	months["May"] = "05"
+	months["June"] = "06"
+	months["July"] = "07"
+	months["August"] = "08"
+	months["September"] = "09"
+	months["October"] = "10"
+	months["November"] = "11"
+	months["December"] = "12"
+}
+
+func main() {
+	files, err := ioutil.ReadDir(sourceDir)
+	if err != nil {
+		panic(err)
+	}
+	numberFiles := len(files)
+
+	for i, file := range files {
+		fmt.Print(fmt.Sprintf("%3d", int(float64(i)/float64(numberFiles)*100)) + "%| " + file.Name())
+
+		filePath := path.Join(sourceDir, file.Name())
+		fd, err := os.Open(filePath)
+		if err != nil {
+			printError(err)
+			continue
+		}
+
+		var created time.Time
+
+		switch strings.ToLower(filepath.Ext(filePath)) {
+		case ".mov":
+			created, err = getVideoCreationTimeMetadata(fd)
+			break
+		case ".xmp":
+			created, err = getXmpCreationTimeMetadata(fd)
+			break
+		default:
+			created, err = sortImage(fd)
+		}
+
+		if err != nil {
+			printError(err)
+			err = fd.Close()
+			if err != nil {
+				printError(err)
+			}
+			continue
+		}
+
+		err = fd.Close()
+		if err != nil {
+			printError(err)
+			continue
+		}
+
+		dstFilePath, err := moveFileToNewLocation(filePath, file.Name(), created)
+		if err != nil {
+			printError(err)
+			continue
+		}
+
+		fmt.Print(" to " + dstFilePath + "\n")
+	}
+
+	showHappyEnd()
+}
+
 func getVideoCreationTimeMetadata(videoBuffer io.ReadSeeker) (time.Time, error) {
 	buf := make([]byte, 8)
 
@@ -45,8 +119,11 @@ func getVideoCreationTimeMetadata(videoBuffer io.ReadSeeker) (time.Time, error) 
 			break // found it!
 		}
 
-		atomSize := binary.BigEndian.Uint32(buf) // check size of atom
-		videoBuffer.Seek(int64(atomSize)-8, 1)   // jump over data and set seeker at beginning of next atom
+		atomSize := binary.BigEndian.Uint32(buf)         // check size of atom
+		_, err := videoBuffer.Seek(int64(atomSize)-8, 1) // jump over data and set seeker at beginning of next atom
+		if err != nil {
+			return time.Time{}, err
+		}
 	}
 
 	// read next atom
@@ -67,12 +144,28 @@ func getVideoCreationTimeMetadata(videoBuffer io.ReadSeeker) (time.Time, error) 
 
 		return time.Unix(appleEpoch-appleEpochAdjustment, 0).Local(), nil
 	case compressedMovieAtomType:
-		return time.Time{}, errors.New("Compressed video")
+		return time.Time{}, errors.New("compressed video")
 	case referenceMovieAtomType:
-		return time.Time{}, errors.New("Reference video")
+		return time.Time{}, errors.New("reference video")
 	default:
-		return time.Time{}, errors.New("Did not find movie header atom (mvhd)")
+		return time.Time{}, errors.New("did not find movie header atom (mvhd)")
 	}
+}
+
+func getXmpCreationTimeMetadata(f io.ReadSeeker) (time.Time, error) {
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return time.Time{}, err
+	}
+	d := &xmp.Document{}
+	if err := xmp.Unmarshal(b, d); err != nil {
+		return time.Time{}, err
+	}
+	model := xmpbase.FindModel(d)
+	if model == nil {
+		return time.Time{}, errors.New("can't get xmp model")
+	}
+	return time.Time(model.CreateDate), nil
 }
 
 func sortImage(f io.ReadSeeker) (time.Time, error) {
@@ -108,74 +201,12 @@ func moveFileToNewLocation(filePath string, fileName string, created time.Time) 
 	return dst, nil
 }
 
-func init() {
-	months["January"] = "01"
-	months["February"] = "02"
-	months["March"] = "03"
-	months["April"] = "04"
-	months["May"] = "05"
-	months["June"] = "06"
-	months["July"] = "07"
-	months["August"] = "08"
-	months["September"] = "09"
-	months["October"] = "10"
-	months["November"] = "11"
-	months["December"] = "12"
+func printError(err error) {
+	fmt.Print("\n")
+	fmt.Println(err)
 }
 
 func showHappyEnd() {
 	fmt.Println()
 	fmt.Println("/   \\          /   \\\n\\_   \\        /  __/\n _\\   \\      /  /__\n \\___  \\____/   __/\n     \\_       _/\n       | @ @  \\_\n       |\n     _/     /\\\n    /o)  (o/\\ \\_\n    \\_____/ /\n      \\____/")
-}
-
-func main() {
-	files, err := ioutil.ReadDir(sourceDir)
-	if err != nil {
-		panic(err)
-	}
-	numberFiles := len(files)
-
-	for i, file := range files {
-		fmt.Print(fmt.Sprintf("%3d", int(float64(i)/float64(numberFiles)*100)) + "%| " + file.Name())
-
-		filePath := path.Join(sourceDir, file.Name())
-		fd, err := os.Open(filePath)
-		if err != nil {
-			fmt.Print("\n")
-			fmt.Println(err)
-			continue
-		}
-
-		var created time.Time
-		if strings.Contains(file.Name(), ".MOV") {
-			created, err = getVideoCreationTimeMetadata(fd)
-			if err != nil {
-				fmt.Print("\n")
-				fmt.Println(err)
-				fd.Close()
-				continue
-			}
-		} else {
-			created, err = sortImage(fd)
-			if err != nil {
-				fd.Close()
-				fmt.Print("\n")
-				fmt.Println(err)
-				continue
-			}
-		}
-
-		fd.Close()
-
-		dstFilePath, err := moveFileToNewLocation(filePath, file.Name(), created)
-		if err != nil {
-			fmt.Print("\n")
-			fmt.Println(err)
-			continue
-		}
-
-		fmt.Print(" to " + dstFilePath + "\n")
-	}
-
-	showHappyEnd()
 }
